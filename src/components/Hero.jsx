db7453d897b8751c39656fa8c3257926d487e2b4
neronalp <scinar38@gmail.com>
2023-06-09 02:11:21 +0300

    v1

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Hero = ({
+  heroapi: { title, subtitle, btntext, img, sociallinks, videos },
+}) => {
+  // console.log(heroapi);
+  return (
+    <>
+      <div className="relative h-auto w-auto flex flex-col">
+        <div className="bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10"></div>
+        <div className="">
+          <div className="">
+            <h1> {title} </h1>
+            <h1> {subtitle} </h1>
+            <button> {btntext} </button>
+            {/* <div className=""></div>
+            <div className=""></div> */}
+          </div>
+          <div className="">
+            <img src={img} alt="hero-img/img" className="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
